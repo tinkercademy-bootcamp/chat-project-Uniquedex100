90129c43ed437147af89de1b8d32185de463a34d
Dinu Goyal <user@ip-172-31-17-176.ap-south-1.compute.internal>
2025-05-28 04:54:57 +0000

    Exercise-3: Answered Bonus Part questions, modified experiment.cc for the same
    Mostly the changes are in experiment.cc where tried and ran different experiments to answer the question in bonus part

--- exercise-3/src/experiment.cc
+++ exercise-3/src/experiment.cc
@@ -14,12 +14,16 @@
 
 using namespace std;
 
-struct temp_struct {
+struct Foo {
   int a;
   std::string b;
-  temp_struct(int k) {
+  Foo(int k) {
     a = k;
     b = "3";
+  }
+  Foo(){
+    a = 5;
+    b = "6";
   }
 };
 
@@ -32,10 +36,30 @@ optional<string> create(bool b){
 signed main(){
   
   string temp_string = "lafjvlajfewlkclknadlsknclksndciweicnlksjcewlkcjwliiewjfijfalijflkewflnewfewkfwoijfijwsklcnkldnnvcklasnlvnlainoifnoiwhfliwlifjksadnksdnvlkndiochipshcowjeifjliwfwlknwlkncklnsdlkvnkwsnflkewnfewlkfjwelkfklewkflnewklnlkdsnclknslkfdflksnflksdnflksnfnsdlksnvlknelkvnleknvlenvrelnvoeifnwivn;lnlknqwlkfnlqwkenflkqwnflkqwnfklsnvjsdnvlkjasnvkjanfvewnvwlincwiajlknv;awncvoiwnlwaevnklwnvienvewlivnhknjkvnlgbneugksdnvuerhnvoifnvurngoiernboiernvierirevnlsivseomvosemvesnvowrivjirjvrpijrdlnvosidvnsdnkjsdvndivnpanvedvneoigaengreingoerirg[eroignjeroigernjgoiegewoignekgw]";
-  temp_struct mystruct(4);
+  Foo mystruct(4);
   optional<string> ret1 = create(false);
   optional<string> ret2 = create(true);
   cout<<"create(false) returned "<<ret1.value_or("empty")<<endl;
   cout<<"create(true) returned "<<ret2.value_or("empty")<<endl;
 
+  uint8_t* p = reinterpret_cast<uint8_t*>(&mystruct);
+  size_t size = sizeof(mystruct);
+  cout<< "Memory layout of mystruct (hex): "<<endl;
+  for(size_t i = 0; i < size; i++){
+    printf("%02X ", p[i]);
+    if ((i + 1) % 8 == 0) cout << endl;
+  }
+
+  // vector<Foo&> hello;
+  // Foo& renamedstruct = mystruct;
+  // hello.push_back(renamedstruct);
+  // for(int i = 0; i < hello.size(); i++){
+  //   cout<<hello[i].a<<endl;
+  // }
+  // cout<<endl;
+
+  vector<optional<Foo>> hello;
+  hello.push_back(Foo{});
+  hello.push_back(nullopt);
+  // nullopt is used to indicate that an optional type does not contain a value.
 }
