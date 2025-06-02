757ae2a60bdf16e63742dd03d45fd01f08cb79ce
Dinu Goyal <user@ip-172-31-17-176.ap-south-1.compute.internal>
2025-06-02 10:56:40 +0000

    Dynamic input based client and basic commands on client side

--- exercise-7/src/net/chat-sockets.cc
+++ exercise-7/src/net/chat-sockets.cc
@@ -12,3 +12,46 @@ sockaddr_in tt::chat::net::create_address(int port) {
   address.sin_port = htons(port);
   return address;
 }
+
+std::string trim(std::string &input){
+    size_t start = input.find_first_not_of(' ');
+    if (start == std::string::npos) return "";
+    size_t end = input.find_last_not_of(' ');
+    return input.substr(start, end - start + 1);
+}
+
+std::string toLower(std::string &input) {
+    std::string result = input;
+    std::transform(result.begin(), result.end(), result.begin(),
+                  [](unsigned char c) { return std::tolower(c); });
+    return result;
+}
+
+bool checkTermination(std::string &input_command){
+  for(auto command : TERMINATION_COMMANDS){
+    if (command == input_command) return true;
+  }
+  return false;
+}
+
+std::string getCommandFromInput(std::string &input_received){
+  std::string input_command = "";
+  for (char character : input_received){
+    if (character == SEPARATION_CHARACTER) break;
+    input_command += character;
+  }
+  input_command = trim(input_command);
+  input_command = toLower(input_command);
+  return input_command;
+}
+
+std::string getMessageFromInput(std::string &input_received){
+  std::string input_message = "";
+  bool flag = false;
+  for (auto character : input_received){
+    if (flag) input_message += character;
+    if (character == SEPARATION_CHARACTER) flag = true;
+  }
+  input_message = toLower(input_message);
+  return input_message;
+}
--- exercise-7/src/net/chat-sockets.h
+++ exercise-7/src/net/chat-sockets.h
@@ -6,15 +6,24 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <string>
+#include <algorithm>
+#include <cctype>
 
 #include "../utils.h"
 
+const std::string TERMINATION_COMMANDS[] = {"exit", "quit", "terminate"};
+const char SEPARATION_CHARACTER = '/';
+
 namespace tt::chat::net {
-
 int create_socket();
-
 sockaddr_in create_address(int port);
-
 } // namespace tt::chat::net
+
+std::string trim(std::string &input);
+std::string toLower(std::string &input);
+bool checkTermination(std::string &input_command);
+std::string getCommandFromInput(std::string &input_received);
+std::string getMessageFromInput(std::string &input_received);
 
 #endif // CHAT_SOCKETS_H
