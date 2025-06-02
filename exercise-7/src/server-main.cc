3ba8c67cd7e45aa929d54ab921b9346103020600
Dinu Goyal <user@ip-172-31-17-176.ap-south-1.compute.internal>
2025-06-02 22:53:17 +0000

    Exercise-7: Epoll in server, can accept multiple connections and handle concurrently

--- exercise-7/src/server-main.cc
+++ exercise-7/src/server-main.cc
@@ -5,15 +5,49 @@
 
 #include "server/chat-server.h"
 #include "server/db-server.h"
+#include "net/chat-sockets.h"
 
 int main() {
   const int kPort = 8080;
-  const int max_clients = 10;
-  // epoll_event events[max_clients];
-
   tt::chat::server::Server server(kPort);
+
+  const int max_clients = 10;
+  epoll_event events[max_clients];
+  char recv_buffer[1024];
+
+  while(true){
+    int nfds = epoll_wait(server.getEpollFd(), events, max_clients, -1);
+    if (nfds == -1){
+      perror("epoll_wait failed");
+      break;
+    }
+
+    for (int i = 0; i < nfds; i++){
+      int fd = events[i].data.fd;
+      if (fd == server.getSocketFd()){
+        server.acceptNewConnection();
+      }
+      else{
+        memset(recv_buffer, 0, 1024);
+        ssize_t bytes_read = read(fd, recv_buffer, sizeof(recv_buffer));
+        if (bytes_read > 0) {
+          SPDLOG_INFO("Received: {}", recv_buffer);
+          send(fd, recv_buffer, bytes_read, 0);  // echo
+          SPDLOG_INFO("Echo message sent");
+        } else if (bytes_read == 0) {
+          SPDLOG_INFO("Client disconnected.");
+          epoll_ctl(server.getEpollFd(), EPOLL_CTL_DEL, fd, nullptr);
+          close(fd);
+        } else {
+          SPDLOG_ERROR("Read error on client socket {}", fd);
+          epoll_ctl(server.getEpollFd(), EPOLL_CTL_DEL, fd, nullptr);
+          close(fd);
+        }
+      }
+    }
+  }
+
   // Database db();
 
-  server.handle_connections();
   return 0;
 }
