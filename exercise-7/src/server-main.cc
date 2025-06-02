42aaa3cdfee02d1c4dc75725e04e7daac0d2aada
Dinu Goyal <user@ip-172-31-17-176.ap-south-1.compute.internal>
2025-06-02 18:31:20 +0000

    Exercise-7: db-server added, consisting of Database and Client Entry classes

--- exercise-7/src/server-main.cc
+++ exercise-7/src/server-main.cc
@@ -4,11 +4,16 @@
 #include <unistd.h>
 
 #include "server/chat-server.h"
+#include "server/db-server.h"
 
 int main() {
   const int kPort = 8080;
+  const int max_clients = 10;
+  // epoll_event events[max_clients];
 
   tt::chat::server::Server server(kPort);
+  // Database db();
+
   server.handle_connections();
   return 0;
 }
