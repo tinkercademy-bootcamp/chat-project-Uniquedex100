9e6cb09d9d3a38436387e9b5ddd850511ea1c073
Dinu Goyal <user@ip-172-31-17-176.ap-south-1.compute.internal>
2025-06-03 04:24:12 +0000

    Exercise-7: Added ncurses, but not using currently; gui needs to be improved

--- exercise-7/src/utils.h
+++ exercise-7/src/utils.h
@@ -4,7 +4,9 @@
 #include <concepts>
 #include <stdexcept>
 #include <string_view>
+#include <string>
 
+#include <ncurses.h>
 #include "spdlog/spdlog.h"
 
 namespace tt::chat {
@@ -15,6 +17,43 @@ void check_error(T test, S error_message) {
     SPDLOG_ERROR("{}", error_message);
     throw std::runtime_error(error_message);
   }
+}
+
+inline void init_ncurses() {
+  initscr();
+  cbreak();
+  noecho();
+  keypad(stdscr, TRUE);
+  scrollok(stdscr, TRUE);
+  curs_set(1);
+  clear();
+  refresh();
+}
+
+inline void end_ncurses() {
+  curs_set(1);
+  endwin();
+}
+
+inline void print_output(const std::string& message) {
+  int input_row = LINES - 2;
+  move(input_row - 1, 0);
+  insertln();
+  printw("%s\n", message.c_str());
+  refresh();
+}
+
+inline std::string get_input() {
+  char input_buffer[256];
+  int input_row = LINES - 2;
+  move(input_row, 0);
+  clrtoeol();
+  printw("> ");
+  refresh();
+  echo();
+  getnstr(input_buffer, sizeof(input_buffer) - 1);
+  noecho();
+  return std::string(input_buffer);
 }
 
 } // namespace tt::chat
